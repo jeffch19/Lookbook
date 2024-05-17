65d814c4b1c3f95fa2dd02df52b64ea07a20cebf
jeffch19 <17jcummings@gmail.com>
2024-05-16 20:34:31 -0400

    intro styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ function Home() {
 
           <div className='intro-content d-flex justify-content-between' >
             <p>Software Engineer <br /> MERN Stack, Cloud Enthusiast</p>
-            <button className='primary-button'> Get Started </button>
+            <button className='primary-button font-bold w-50'> Get Started </button>
           </div>
         </div>
 
