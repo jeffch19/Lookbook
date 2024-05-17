d552fad62e90a3f76a98270eac20bfbc051ff077
jeffch19 <17jcummings@gmail.com>
2024-05-17 14:53:22 -0400

    header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { RiMenu3Fill, RiCloseFill } from "react-icons/ri";
+import { Link } from 'react-router-dom';
 
 
 function Header() {
@@ -14,6 +15,13 @@ function Header() {
         onClick={() => {setShowHeader(!showHeader)}}
         className='menu-icon position-fixed top-0 end-0' />)}
 
+
+        <ul className={`${ showHeader  ? 'show-header' : 'hide-header'} n-box1`}>
+          <li><Link>Home</Link></li>
+          <li><Link>Projects</Link></li>
+          <li><Link>About Me</Link></li>
+          <li><Link>Contact</Link></li>
+        </ul>
     </div>
   )
 
