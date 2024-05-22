888ea2db9deac21eb67e0ed3368950238b5c70a8
jeffch19 <17jcummings@gmail.com>
2024-05-21 20:12:50 -0400

    starting to add certs and recs

---
+++ src/pages/certdata.js
@@ -0,0 +1,19 @@
+const certdata = [
+  {
+    title: "AWS Certified Cloud Practitioner",
+    image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--V068QLqC--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/jbux8kukb9osdpqwc2sm.png",
+
+    link: "https://www.credly.com/badges/ec2a5969-e79e-4d0d-acfa-9e6d00604d5d/email" ,
+  },
+  {
+    title: "University of Pennsylvania Full Stack Coding Certificate",
+    image:
+      "https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/686461/cover-secure-rest-api-in-nodejs-18f43b3033c239da5d2525cfd9fdc98f.png",
+
+    link: "https://scl.io/3BHypE2",
+  },
+  
+];
+
+export default certdata;
